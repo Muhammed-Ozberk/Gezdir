fd983de911cd7dd698df0b31ce0f700b9154b79d
MuhammedOzberk <ozberkmuhammed@gmail.com>
2023-08-02 16:37:20 +0300

    api key protection change

--- app/src/main/java/com/example/gezdir/services/FcmApiService.kt
+++ app/src/main/java/com/example/gezdir/services/FcmApiService.kt
@@ -1,8 +1,8 @@
 package com.example.gezdir.services
 
+import com.example.gezdir.BuildConfig
 import com.example.gezdir.data.entity.FcmNotification
 import com.example.gezdir.data.entity.FcmResponse
-import com.example.gezdir.util.ApiKeyHelper
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
@@ -12,7 +12,7 @@ interface FcmApiService {
     @POST("fcm/send")
     fun sendNotification(
         @Header("Content-Type") contentType: String = "application/json",
-        @Header("Authorization") authorization: String = "key=" + ApiKeyHelper.getFcmApiKey(),
+        @Header("Authorization") authorization: String = "key=" + BuildConfig.FCM_API_KEY,
         @Body notification: FcmNotification
     ): Call<FcmResponse>
 }
